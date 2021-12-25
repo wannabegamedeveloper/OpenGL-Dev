aea98cb3498cfec6043a4adab2a711ec8ebb5310
TusharVaid30 <31384470+TusharVaid30@users.noreply.github.com>
2021-12-25 23:19:14 +0530

    Triangles

--- graphics.cpp
+++ graphics.cpp
@@ -1,7 +1,27 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 static const int _WIDTH = 1920;
 static const int _HEIGHT = 1080;
+static const char* _TITLE = "Game";
+
+GLuint VAO, VBO;
+GLuint shaderProgram;
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+
+"void main()\n"
+"{\n"
+"    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
 
 void GLFWHints()
 {
@@ -10,17 +30,69 @@ void GLFWHints()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 }
 
-GLFWwindow* CreateWindow(int WIDTH, int HEIGHT)
+void CreateTriangle()
 {
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Test", NULL, NULL);
+    GLfloat vertices[] =
+    {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+
+
+GLFWwindow* CreateWindow(int WIDTH, int HEIGHT, const char* TITLE)
+{
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, TITLE, NULL, NULL);
     if (window == NULL) return NULL;
 
     glfwMakeContextCurrent(window);
 
-    while (!glfwWindowShouldClose(window))
-        glfwPollEvents();
-
     return window;
+}
+
+void SetWindowColor(GLfloat r, GLfloat g, GLfloat b, GLFWwindow* window)
+{
+    gladLoadGL();
+
+    glViewport(1920/3, 1080/3, 1920, 1080);
+
+    glClearColor(r, g, b, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
 }
 
 int main(void)
@@ -29,7 +101,22 @@ int main(void)
 
     GLFWHints();
 
-    GLFWwindow* window = CreateWindow(_WIDTH, _HEIGHT);
+    GLFWwindow* window = CreateWindow(_WIDTH, _HEIGHT, _TITLE);
+
+    SetWindowColor(0.3f, 0.4f, 0.2f, window);
+
+    CreateTriangle();
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
     glfwDestroyWindow(window);
     
